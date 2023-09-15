4b7732b4dc6fdae2046a62d5a475b71837f10637
shadasali <ssakib4550@townsendharris.org>
2023-09-15 13:10:20 -0400

    feeling feature functional in WOM-Popup

--- SocialMediaApp/src/WhatsOnYourMind.js
+++ SocialMediaApp/src/WhatsOnYourMind.js
@@ -3,11 +3,159 @@ import { Link } from 'react-router-dom';
 import axios from "axios";
 import './WhatsOnYourMind.css';
 
-function EmojisModal({emojis}){
+function EmojisModal({emojis, onClose, onSelectEmoji}){
+    const [searchQuery, setSearchQuery] = useState('');
+    const handleBackButton = () => {
+        onClose();
+      };
+      // Mapping between emoji labels and indices in the emojis array
+  const emojiLabelsToIndices = {
+    happy: 8,
+    relaxed: 50,
+    loved: 14,
+    sad:75,
+    thankful:2,
+    excited:16,
+    'in love':15,
+    crazy: 26,
+    blessed:13,
+    blissful:12,
+    fantasic: 3,
+    silly: 27,
+    festive: 67,
+    cool:69,
+    tired:97,
+    sick:56,
+    different:68,
+    smart:70,
+    confused:73,
+    worried:74, 
+    shocked:77,
+    scared:85,
+    cold:62,
+    embarrassed:32,
+    sleepy:54,
+    hot:61,
+    curious:34,
+    rich:28,
+    relieved:47,
+    disgusted:58,
+    terrified:90,
+    'in pain':92,
+    disappointed:93,
+    angry:99,
+    hilarious:89,
+    emotional:82,
+    nostalgic:82,
+    anxious:86,
+    grief:88,
+    alone:93, 
+    proud:29,
+    secretive:36,
+    motivated:1,
+    inquistive:71,
+    injured:57,
+    amazed:79,
+    'wiped out':64,
+    heartbroken:81,
+    unamused:44,
+    guilty:51,
+    contagious:55,
+    hungry:53,
+    skeptical:37,
+    bored:39,
+    indifferent:38,
+    annoyed:44,
+    'lied to':48,
+    lost:41,
+    secluded:42,
+    speechless:40,
+    awkward:46,
+    important:50,
+    allergic:60,
+    nervous:80,
+    flushed:80,
+    drunk:63,
+    weary:95,
+    fuming:98,
+    unimportant:93,
+    hopeful:649,
+    evil:102,
+    funny:107,
+    scary:110,
+    secure:1290,
+    graduated:1147,
+    engaged:1153,
+    strong:452,
+    married:128,
+    stinky:582,
+    nocturnal:598,
+    artistic:314,
+    lucky:665,
+    super:367,
+    inspired:1216,
+    special:16,
+    starstruck:16,
+    small:76,
+    beautiful:392,
+    upset:100,
+    irritated:100,
+    mischievious:43,
+    satisfied:50,
+    cute:12,
+    goofy: 26,
+    obedient:35,
+    concerned:37,
+    calm:50,
+    sarcastic:43,
+    hyper:26,
+    puzzled:72,
+    'pissed off':101,
+    surprised:79,
+    frustrated:100,
+    refreshed:1,
+    meh:38,
+    better:2,
+    indecisive:277,
+    suspicious:242,
+  };
+
+  // Function to handle user's input in the search bar
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value.toLowerCase());
+  };
+
+  // Filter the emoji labels based on the user's input
+  const filteredEmojiLabels = Object.keys(emojiLabelsToIndices).filter((label) =>
+    label.includes(searchQuery)
+  );
+
+  // Organize emojis into rows with a maximum of three emojis per row
+  const rows = [];
+  let currentRow = [];
+  filteredEmojiLabels.forEach((label) => {
+    currentRow.push(label);
+    if (currentRow.length === 3) {
+      rows.push(currentRow);
+      currentRow = [];
+    }
+  });
+  if (currentRow.length > 0) {
+    rows.push(currentRow);
+  }
+
+  const handleEmojiClick = (label) => {
+    const selectedEmoji = {
+      text: label,
+      character: emojis[emojiLabelsToIndices[label]].character
+    };
+    onSelectEmoji(selectedEmoji);
+    onClose();
+  };
     return(
         <div className='emoji-popup'>
             <div className="header" style={{display:'flex', alignItems:'center'}}>
-                <button className="back-button">
+                <button className="back-button" onClick={handleBackButton}>
                     <div className="back">
                         <img src="/back-icon.png" alt="" className="back-icon" width="25" height="25" />
                     </div>
@@ -19,241 +167,28 @@ function EmojisModal({emojis}){
                 <input
                     type="text"
                     placeholder="&#128269;  Search"
-                    className="form-control rounded-pill"   
+                    className="form-control rounded-pill"  
+                    value={searchQuery}
+                    onChange={handleSearchInputChange} 
                 />
             </div>
-            <div className="row1">
-                <div className="happy-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[8].character}
-                        </div>
-                        <div className="emoji-button-text">happy</div>
+            {rows.length > 0 ? (
+            rows.map((row, rowIndex) => (
+                <div key={rowIndex} className={`row${rowIndex + 1}`} style={{display:'flex'}}>
+                {row.map((label) => (
+                    <div key={label} className={`${label}-emoji-button`}>
+                    <button className="emoji-button"  onClick={() => handleEmojiClick(label)}>
+                        <div className="emoji">{emojis[emojiLabelsToIndices[label]].character}</div>
+                        <div className="emoji-button-text">{label}</div>
                     </button>
-                    <span/>
+                    <span />
+                    </div>
+                ))}
                 </div>
-                <div className="relieved-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[50].character}
-                        </div>
-                        <div className="emoji-button-text">relieved</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="loved-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[14].character}
-                        </div>
-                        <div className="emoji-button-text">loved</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row2">
-                <div className="sad-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[75].character}
-                        </div>
-                        <div className="emoji-button-text">sad</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="thankful-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[2].character}
-                        </div>
-                        <div className="emoji-button-text">thankful</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="excited-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[16].character}
-                        </div>
-                        <div className="emoji-button-text">excited</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row3">
-                <div className="in-love-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[15].character}
-                        </div>
-                        <div className="emoji-button-text">in love</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="crazy-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[26].character}
-                        </div>
-                        <div className="emoji-button-text">crazy</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="blessed-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[13].character}
-                        </div>
-                        <div className="emoji-button-text">blessed</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row4">
-                <div className="blissful-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[12].character}
-                        </div>
-                        <div className="emoji-button-text">blissful</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="fantastic-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[3].character}
-                        </div>
-                        <div className="emoji-button-text">fantastic</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="silly-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[27].character}
-                        </div>
-                        <div className="emoji-button-text">silly</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row5">
-                <div className="celebratory-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[67].character}
-                        </div>
-                        <div className="emoji-button-text">festive</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="cool-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[69].character}
-                        </div>
-                        <div className="emoji-button-text">cool</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="tired-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[97].character}
-                        </div>
-                        <div className="emoji-button-text">tired</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row6">
-                <div className="sick-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[56].character}
-                        </div>
-                        <div className="emoji-button-text">sick</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="different-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[68].character}
-                        </div>
-                        <div className="emoji-button-text">different</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="smart-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[70].character}
-                        </div>
-                        <div className="emoji-button-text">smart</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row7">
-                <div className="confused-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[73].character}
-                        </div>
-                        <div className="emoji-button-text">confused</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="worried-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[74].character}
-                        </div>
-                        <div className="emoji-button-text">worried</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="shocked-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[77].character}
-                        </div>
-                        <div className="emoji-button-text">shocked</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
-            <div className="row8">
-                <div className="embarrassed-emoji-button">
-                    <button className="embarrassed-emoji-button">
-                        <div className="emoji">
-                            {emojis[32].character}
-                        </div>
-                        <div className="emoji-button-text">embarrassed</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="scared-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[85].character}
-                        </div>
-                        <div className="emoji-button-text">scared</div>
-                    </button>
-                    <span/>
-                </div>
-                <div className="cold-emoji-button">
-                    <button className="emoji-button">
-                        <div className="emoji">
-                            {emojis[62].character}
-                        </div>
-                        <div className="emoji-button-text">cold</div>
-                    </button>
-                    <span/>
-                </div>
-            </div>
+            ))
+            ):(
+                <p className="gray-text d-flex justify-content-center" style={{marginTop:'25px'}}>Nothing found</p>
+            )}
         </div>
     );
 }
@@ -266,6 +201,7 @@ function WhatsOnYourMind ({onClose}){
     const fileInputRef = useRef(null);
     const [emojis, setEmojis] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedEmoji, setSelectedEmoji] = useState(null);
 
     const handleTextareaChange = (event) => {
         setPostContent(event.target.value);
@@ -304,6 +240,10 @@ function WhatsOnYourMind ({onClose}){
 
       };
 
+      const handleEmojiSelect = (emoji) => {
+        setSelectedEmoji(emoji);
+      };
+
     return(
         <div className="whats-on-your-mind-popup">
             <button className="close-button" onClick={onClose}>
@@ -316,8 +256,13 @@ function WhatsOnYourMind ({onClose}){
                     </Link>
                 </div>
                 <div className="userName" style={{marginLeft:'10px', fontWeight:'bold'}}>
-                    {`${firstnameUser} ${lastnameUser}`}
+                    {`${firstnameUser} ${lastnameUser}`} 
                 </div>
+                {selectedEmoji && (
+                    <div className="feeling-status" style={{marginLeft:'2px', fontWeight:'bold'}}>
+                    is {selectedEmoji.character} feeling {selectedEmoji.text}. 
+                    </div>
+                )}
             </div>
             <div className="input-field">
             <textarea
@@ -355,7 +300,7 @@ function WhatsOnYourMind ({onClose}){
                     </button>
                     <span className="tooltip-feel" style={{fontSize:'12px'}}>Feeling</span>
                 </div>
-                {isModalOpen && <EmojisModal emojis={emojis}/>}
+                {isModalOpen && <EmojisModal emojis={emojis} onClose={() => setIsModalOpen(false)} onSelectEmoji={handleEmojiSelect}/>}
                 <div className="location-button">
                     <button className="location-button btn" 
                         style={{border:'gray'}}
